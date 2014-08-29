7e02afeed041d95244eb6be4d2f8dca6683cee08
Dmitry Ryzhov <dryzhov@design.ru>
2014-08-29 11:41:44 +0400

    added package.json

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var connect = require('connect'),
     directory = './';
 
 connect()
-    .use(connect.static(directory))
-    .listen(3000);
+  .use(connect.static(__dirname + '/.')).listen(3000);
 
 console.log('Listening on port 3000');
